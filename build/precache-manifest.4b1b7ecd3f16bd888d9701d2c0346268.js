cca49561953b14e855c1f17cd05420930e63b57f
schieberan <54329383+schieberan@users.noreply.github.com>
2020-10-15 15:45:05 -0400

    Add files via upload

---
+++ build/precache-manifest.4b1b7ecd3f16bd888d9701d2c0346268.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50ca9b4f4504b2c82cf7d524874456ed",
+    "url": "/black-dashboard-react/index.html"
+  },
+  {
+    "revision": "80235c43cf191580d556",
+    "url": "/black-dashboard-react/static/css/main.401ec201.chunk.css"
+  },
+  {
+    "revision": "855d934c21a3dd912f71",
+    "url": "/black-dashboard-react/static/js/2.c991ff17.chunk.js"
+  },
+  {
+    "revision": "9b707ee842af4f317f626a3c3900dd73",
+    "url": "/black-dashboard-react/static/js/2.c991ff17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80235c43cf191580d556",
+    "url": "/black-dashboard-react/static/js/main.b511a0a1.chunk.js"
+  },
+  {
+    "revision": "dc192b57bfed2afd6be1ea5558f17255",
+    "url": "/black-dashboard-react/static/js/main.b511a0a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22574e9ecc9b72002c07",
+    "url": "/black-dashboard-react/static/js/runtime-main.d55d70f9.js"
+  },
+  {
+    "revision": "bd6820f1a4015e9645e470ea8e7b1501",
+    "url": "/black-dashboard-react/static/media/anime3.bd6820f1.png"
+  },
+  {
+    "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
+    "url": "/black-dashboard-react/static/media/emilyz.9fcf69e5.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/black-dashboard-react/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/black-dashboard-react/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/black-dashboard-react/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/black-dashboard-react/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "eb6be414b75c412c13eeee2d2e8390cb",
+    "url": "/black-dashboard-react/static/media/react-logo.eb6be414.png"
+  }
+]);
